3f61d3ac2c718d467ec14f2839ecdf541dc2a596
Eloi Cañigueral <95910674+eloicanigueral@users.noreply.github.com>
2026-08-08 16:15:50 +0000

    Clean code & create NOTES.md to my notes of research or interest...

--- popup/popup.js
+++ popup/popup.js
@@ -16,18 +16,6 @@ function openVideo(videoUrl, newTab=false){ //function to avoid repeating the sa
 
 function pickRandomVideo(){
   //this is gonna run inside youtube page, so i cannot use anything about the popup.html....
-  
-
-  //i figured (inspecting the yt WL page) that all the videos are in blocks/elements named: "ytd-playlist-video-renderer"
-  //and if i type: document.querySelectorAll('ytd-playlist-video-renderer') in the console i'm able to see all the videos (well.. not all, only the first 100..., have to fix that) -> if i scroll down a little it fixes so im gonna have to put a mini scroll in the script? or somethig
-  //if i type the same .length i can see how many videos are there... (to choose a random number bettween 0 and that number)
-
-  //i keep investigating (and searching on google...) and now i know that once i have the video i want ( document.querySelectorAll('ytd-playlist-video-renderer')[n] ) i can get easily the link adding: .querySelector('a#video-title').href 
-        //explanation: the link is always in the href on the a "block", but there are different a blocks, the one we are interested in is the video-title one
-        // so it would end up like this: document.querySelectorAll('ytd-playlist-video-renderer')[n].querySelector('a#video-title').href    || where n is a random number between 0 and document.querySelectorAll('ytd-playlist-video-renderer').length
-        //ok.. i've changed the language and now video-title doesnt appear.. i suppose i have to use only a whithout any id name...
-            // i could use: 'a[href*="/watch"]' -> this searches one that has got /watch in the href.. (/watch opens automatically a video...)
-
 
     //and i should make the scroll also.. butt to be do it later when all this works (it seems i dont need it... have tocheck)
   let videos = document.querySelectorAll('ytd-playlist-video-renderer, ytd-playlist-panel-video-renderer');
