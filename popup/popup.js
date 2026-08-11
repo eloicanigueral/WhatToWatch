a7911142d6fc59f9485de29fd36d243e68587f2b
eloicanigueral <eloi.canigueral@gmail.com>
2026-08-11 17:29:48 +0200

    Delete all related to checkbox (bc of the problems)

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,4 @@
 const button = document.getElementById('pickRandomVideo');
-const chbx = document.getElementById('watchLaterCheckbox')
 const statusText = document.getElementById('status');
 
 
@@ -58,7 +57,6 @@ button.addEventListener('click', function() { // when clicking the main button:
     
   statusText.innerText = "Thinking.... ||| Working... "; //I THINK I HAVE TO DELETE THIS STATUS MSG (OR AT LEAST MODIFY IT....)
 
-  //check when the checkbox has to be shown.. and how to do that! --> okey.. definetly not here, bc this only runs when the button is clicked, so it has to be done before, but dk if here in the js or in the html or what...
   browser.tabs.query({active: true, currentWindow: true})
     .then(function(tabs){  //it can be better and cleaner if i use .then(openVid, onError); and then creating two differents functions outside... 
       
@@ -69,36 +67,26 @@ button.addEventListener('click', function() { // when clicking the main button:
   
         if (tab.url.includes("list=")){ //if we're on a playlist page, the url has something like: list=... //ARE WE IN A PLAYLIST?
           
-          //MOST DIFFICULT I THINK.. TO DO LATER ON... but if we're in the WL playlist, the checkbox shouldnt appear.... !!!!!!!!!!!!!!!
 
-          if (chbx.checked){  //if it has appeared we shouldnt be in WL page (have to do that..)
-            //we are in a playlist, but user wants WL (because the checkbox is active)
+          //pick random video from the current playlist
 
-            openVideo(defaultUrl, false) //opened on the same page and from the WL
-            .then(loadingTab);
-
-            statusText.innerText = "Playing random video from the Watch Later list";
-
+          //we are already in a page with the list/playlist uploaded, so isnt necessary to load enterily a new page...
+          browser.scripting.executeScript({ //it exectues the function func in the target (which is the yt page (w the playlist))
+            target: { tabId: tab.id },
+            func: pickRandomVideo
+          }).then(function(link) {
             
-          } else { //pick random video from the current playlist
+            if (link[0].result === null){
+              statusText.innerText = "No video found, check if the playlist isn't empty and try again";
+            } else {
+              openVideo(link[0].result); //now it has to open in the same page, that's why there's not the 'true'        
+              statusText.innerText = "Video correctly loaded!";
+            openVideo(link[0].result); //now it has to open in the same page, that's why there's not the 'true'  
+            }
+          });
+          //statusText.innerText = "Playing random video from this playlist";
 
-            //we are already in a page with the list/playlist uploaded, so isnt necessary to load enterily a new page...
-            browser.scripting.executeScript({ //it exectues the function func in the target (which is the yt page (w the playlist))
-              target: { tabId: tab.id },
-              func: pickRandomVideo
-            }).then(function(link) {
-              
-              if (link[0].result === null){
-                statusText.innerText = "No video found, check if the playlist isn't empty and try again";
-              } else {
-                openVideo(link[0].result); //now it has to open in the same page, that's why there's not the 'true'        
-                statusText.innerText = "Video correctly loaded!";
-              openVideo(link[0].result); //now it has to open in the same page, that's why there's not the 'true'  
-              }
-            });
-            //statusText.innerText = "Playing random video from this playlist";
-
-          }
+          
   
         } else{ //we are on youtube, but not in any playlist -> random video from WL
           openVideo(defaultUrl, false)
